dc819b67471fa468ceb3f5b3118665a7f15ca941
MehtaShivam <mehta.shivam95@gmail.com>
2023-09-08 18:32:16 -0400

    Changed Budget.js for max and min values

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,10 +2,23 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch, remaining } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        console.log(remaining);
+        if(event.target.value > 20000){
+            alert("Cannot be more than 20000");
+
+        }else if (remaining <= 0){
+            alert("Cannot be less than spent");
+        }
+        else{
+            setNewBudget(event.target.value);
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: event.target.value,
+            })
+        }
     }
     return (
 <div className='alert alert-secondary'>
